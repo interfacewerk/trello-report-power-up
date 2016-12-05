7538af1503280cb1478b863cd3b92e4ac2f0579d
Sebastian Ullherr <sebastian-git@intefacewerk.de>
2016-12-05 17:01:36 +0100

    clean up and improve logo

--- js/template.js
+++ js/template.js
@@ -11,50 +11,13 @@ var boardButtonCallback = function(t){
   });
 };
 
-var cardButtonCallback = function(t){
-  var items = Object.keys(parkMap).map(function(parkCode){
-    var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
-    return {
-      text: parkMap[parkCode],
-      url: urlForCode,
-      callback: function(t){
-        return t.attach({ url: urlForCode, name: parkMap[parkCode] })
-        .then(function(){
-          return t.closePopup();
-        })
-      }
-    };
-  });
-
-  return t.popup({
-    title: 'Popup Search Example',
-    items: items,
-    search: {
-      count: 5,
-      placeholder: 'Search National Parks',
-      empty: 'No parks found'
-    }
-  });
-};
-
 TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [{
       icon: WHITE_ICON,
-      text: 'Template',
+      text: 'Business Report',
       callback: boardButtonCallback
     }];
-  },
-  'format-url': function(t, options) {
-    var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
-      return {
-        icon: GRAY_ICON,
-        text: parkName
-      };
-    } else {
-      throw t.NotHandled();
-    }
   },
   'show-settings': function(t, options){
     return t.popup({
